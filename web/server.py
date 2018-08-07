e51e4ceee739d348383f7f67eb4959292287f55e
Michele Proverbio <proch92@gmail.com>
2018-08-07 18:01:39 +0200

    impostazione progetto jinja2

--- web/server.py
+++ web/server.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def redirect():
-	return app.send_static_file('content.html')
+def curr():
+	f = open('content.json')
+	data = json.load(f)
+	return render_template('content.html', data=data)
 
 if __name__ == "__main__":
 	app.run(host= '127.0.0.1', port=5003)
